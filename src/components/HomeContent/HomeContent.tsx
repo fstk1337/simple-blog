b0a3eae0a4fae4221e4409b6491a285c0f51c1d8
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2023-12-23 10:35:56 +0300

    feat: add viewPosts button; add sidebar

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -13,9 +13,26 @@ export const HomeContent = () => {
             {cards.map(card =>
               <PostCard key={card.id} card={card} />
             )}
+            <button type="button" className={style.viewPostsBtn}>View all posts</button>
           </div>
           <div className={style.sideColumn}>
-            Side Column
+            <div className={style.sidebar}>
+              <div className={style.sidebarSearch}>
+                <input type="search" placeholder='type to search...' />
+              </div>
+              <div className={style.sidebarItem}>
+                <div className={style.sidebarHeading}>Recent posts</div>
+                <div className={style.sidebarContent}>Content recent posts</div>
+              </div>
+              <div className={style.sidebarItem}>
+                <div className={style.sidebarHeading}>Categories</div>
+                <div className={style.sidebarContent}>Content categories</div>
+              </div>
+              <div className={style.sidebarItem}>
+                <div className={style.sidebarHeading}>Tag clouds</div>
+                <div className={style.sidebarContent}>Content tag clouds</div>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
