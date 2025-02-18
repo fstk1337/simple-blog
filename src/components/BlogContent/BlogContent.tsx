b8c6632d72fe885f325a82d69c8ca0f0e23a62fd
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2025-02-18 21:03:56 +0300

    feat: add pagination to blog page

--- src/components/BlogContent/BlogContent.tsx
+++ src/components/BlogContent/BlogContent.tsx
@@ -5,18 +5,50 @@ import { Sidebar } from '@components/Sidebar';
 import { cards } from '@constants/cards';
 
 import style from './BlogContent.module.scss';
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
 
 export const BlogContent = () => {
+  const [page, setPage] = useState(0);
+  const [posts] = useState(cards);
+  const postsPerPage = 6;
+  const totalPages = Math.ceil(cards.length / postsPerPage);
+
+  const displayPosts = posts.slice(
+    page * postsPerPage,
+    page * postsPerPage + postsPerPage
+  ).map(post => {
+    return (
+      <div key={post.id} className={style.cardWrapper}>
+        <PostCard card={post} isShort={true} />
+      </div>
+    );
+  });
+
+  const changePage = (obj: { selected: number;}) => {
+    setPage(obj.selected);
+  };
+
   return (
     <section className={style.blogContent}>
       <Container>
         <div className={style.content}>
           <div className={style.mainColumn}>
-            {cards.map(card =>
-              <div key={card.id} className={style.cardWrapper}>
-                <PostCard card={card} isShort={true} />
+            <div className={style.posts}>{displayPosts}</div>
+            <div className={style.pagination}>
+              <ReactPaginate
+                pageCount={totalPages}
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                onPageChange={changePage}
+                containerClassName={style.paginationBtns}
+                previousLinkClassName={"prevBtn"}
+                nextLinkClassName={"nextBtn"}
+                disabledClassName={"navDisabled"}
+                activeClassName={"navActive"}
+              />
             </div>
-            )}
           </div>
           <div className={style.sideColumn}>
             <Sidebar />
--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -33,7 +33,7 @@ export const cards: Card[] = [
     author: 'Admin',
     date: 'May 24, 2020',
     image: post2,
-    comments: 36,
+    comments: 26,
     description: 'You can support us by contributing a little via PayPal. Please contact TemplateMo via Live Chat or Email. If you have any question or feedback about this template, feel free to talk to us. Also, you may check other CSS templates such as multi-page, resume, video, etc.',
     tags: ['Best Templates', 'TemplateMo']
   },
@@ -44,7 +44,95 @@ export const cards: Card[] = [
     author: 'Admin',
     date: 'May 14, 2020',
     image: post3,
-    comments: 48,
+    comments: 42,
+    description: 'Nullam at quam ut lacus aliquam tempor vel sed ipsum. Donec pellentesque tincidunt imperdiet. Mauris sit amet justo vulputate, cursus massa congue, vestibulum odio. Aenean elit nunc, gravida in erat sit amet, feugiat viverra leo. Phasellus interdum, diam commodo egestas rhoncus, turpis nisi consectetur nibh, in vehicula eros orci vel neque.',
+    tags: ['HMTL CSS', 'Photoshop']
+  },
+  {
+    id: 4,
+    topic: 'lifestyle',
+    title: 'Mauris ac dolor ornare',
+    author: 'Admin',
+    date: 'May 16, 2020',
+    image: post1,
+    comments: 28,
+    description: 'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.',
+    tags: ['God Damn', 'Selfish']
+  },
+  {
+    id: 5,
+    topic: 'healthy',
+    title: 'Donec tincidunt leo',
+    author: 'Admin',
+    date: 'May 12, 2020',
+    image: post3,
+    comments: 16,
+    description: 'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.',
+    tags: ['Beauty', 'Friendly']
+  },
+  {
+    id: 6,
+    topic: 'fashion',
+    title: 'Mauris ac dolor ornare',
+    author: 'Admin',
+    date: 'May 10, 2020',
+    image: post2,
+    comments: 3,
+    description: 'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.',
+    tags: ['Cool', 'Mystic']
+  },
+  {
+    id: 7,
+    topic: 'healthy',
+    title: 'Etiam id diam vitae lorem dictum',
+    author: 'Admin',
+    date: 'May 24, 2020',
+    image: post2,
+    comments: 26,
+    description: 'You can support us by contributing a little via PayPal. Please contact TemplateMo via Live Chat or Email. If you have any question or feedback about this template, feel free to talk to us. Also, you may check other CSS templates such as multi-page, resume, video, etc.',
+    tags: ['Best Templates', 'TemplateMo']
+  },
+  {
+    id: 8,
+    topic: 'fashion',
+    title: 'Donec tincidunt leo nec magna',
+    author: 'Admin',
+    date: 'May 14, 2020',
+    image: post3,
+    comments: 42,
+    description: 'Nullam at quam ut lacus aliquam tempor vel sed ipsum. Donec pellentesque tincidunt imperdiet. Mauris sit amet justo vulputate, cursus massa congue, vestibulum odio. Aenean elit nunc, gravida in erat sit amet, feugiat viverra leo. Phasellus interdum, diam commodo egestas rhoncus, turpis nisi consectetur nibh, in vehicula eros orci vel neque.',
+    tags: ['HMTL CSS', 'Photoshop']
+  },
+  {
+    id: 9,
+    topic: 'lifestyle',
+    title: 'Mauris ac dolor ornare',
+    author: 'Admin',
+    date: 'May 16, 2020',
+    image: post1,
+    comments: 28,
+    description: 'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.',
+    tags: ['God Damn', 'Selfish']
+  },
+  {
+    id: 10,
+    topic: 'healthy',
+    title: 'Etiam id diam vitae lorem dictum',
+    author: 'Admin',
+    date: 'May 24, 2020',
+    image: post2,
+    comments: 26,
+    description: 'You can support us by contributing a little via PayPal. Please contact TemplateMo via Live Chat or Email. If you have any question or feedback about this template, feel free to talk to us. Also, you may check other CSS templates such as multi-page, resume, video, etc.',
+    tags: ['Best Templates', 'TemplateMo']
+  },
+  {
+    id: 11,
+    topic: 'fashion',
+    title: 'Donec tincidunt leo nec magna',
+    author: 'Admin',
+    date: 'May 14, 2020',
+    image: post3,
+    comments: 42,
     description: 'Nullam at quam ut lacus aliquam tempor vel sed ipsum. Donec pellentesque tincidunt imperdiet. Mauris sit amet justo vulputate, cursus massa congue, vestibulum odio. Aenean elit nunc, gravida in erat sit amet, feugiat viverra leo. Phasellus interdum, diam commodo egestas rhoncus, turpis nisi consectetur nibh, in vehicula eros orci vel neque.',
     tags: ['HMTL CSS', 'Photoshop']
   }
